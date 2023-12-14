28ed3a6af7fdc974712ba3144a781a045d46361e
Jonathanpv99 <jonyvegapjv@gmail.com>
2023-12-14 17:41:55 -0600

    feat: add Navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,37 @@
-
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMountainSun } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
 
     return (
-        <div>
-        <h1>Navbar chidote</h1>
-        </div>
+        <nav className="bg-azul text-azul my-3 py-5 px-10 rounded-lg flex justify-between">
+            <div>
+                <Link to='/'>
+                <div className='flex gap-x-4 items-center text-white'>
+                    <FontAwesomeIcon  icon={ faMountainSun }/>
+                    <h1 className='text-2xl font-bold'>NORTWHIND REPORTS</h1>
+                </div>
+                
+                </Link>
+                
+            </div>
+            <div>
+                <ul className='flex gap-x-20 items-center'>
+                    <li className='bg-verde-b rounded-lg p-2'>
+                    <Link to='/products' className='font-bold'> Products/Quarter </Link>
+                    </li>
+                    <li className='bg-verde-b rounded-lg p-2'>
+                    <Link to='/period' className='font-bold'> Sales/Period </Link>
+                    </li>
+                    <li className='bg-verde-b rounded-lg p-2'>
+                        <Link to='/mensual' className='font-bold'> Monthly Sales </Link>
+                    </li>
+                    <li className='bg-cyan-600 rounded-lg p-2'>
+                        <Link to='/team' className='font-bold'> Team </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
